0f01b46c583f3a3ac87c8a2e21a02e58d221946f
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-03-31 01:37:19 +0300

    modified sandbox/c.py

--- Exam_Kontur/sandbox/c.py
+++ Exam_Kontur/sandbox/c.py
@@ -1,20 +1,31 @@
+d = {'ZENIT': 65, 'SOCHI': 56, 'DINAMO': 53, 'CSKA': 50, 'KRASNODAR': 49, 'LOKOMOTIV': 48, 'AKHMAT': 41}
+lst = [65, 56, 53, 50, 49, 48, 41]
+match = 'KRASNODAR-AKHMAT'
+
+def print_index(const_lst: list, num: int) -> None:
+    lst = const_lst[:]
+    lst.append(num)
+    lst.sort()
+    print(len(lst) - lst.index(num), end=' ')
+
+
 # num = int(input())
+# if  num < 2 or num > 20:
+#     quit()
 # d = {}
 # lst = []
 # for i in range(num):
 #     a, b = input().split()
-#     d[a] = int(b)
+#     if len(a) < 3 or len(a) > 12:
+#         quit()
+#     d[a.upper()] = int(b)
 #     lst.append(int(b))
-# match = input()
-# print(d)
-# print(lst)
-d = {'ZENIT': 65, 'SOCHI': 56, 'DINAMO': 53, 'CSKA': 50, 'KRASNODAR': 49, 'LOKOMOTIV': 48, 'AKHMAT': 41}
-lst = [65, 56, 53, 50, 49, 48, 41]
-match = 'KRASNODAR-AKHMAT'
-match = match[:match.find("-")]
-print(match, d[match])
-new_num = d[match] + 3
-lst.append(new_num)
-lst.sort()
-lst.reverse()
-print(lst)
+# match = input().upper()
+team1 = match[:match.find("-")]
+team2 = match[match.find("-") + 1 :]
+print(team1, team2)
+lst2 = lst[:]
+lst2.remove(d[team1])
+print_index(lst2, d[team1] + 3)
+print_index(lst2, d[team1] + 1)
+print(lst.index(d[team1]) + 1)
