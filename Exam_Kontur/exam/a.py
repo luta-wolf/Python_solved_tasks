83793bc324c96750df306523d6257f315dc53ab8
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-04-02 18:24:49 +0300

    add a.py exam

---
+++ Exam_Kontur/exam/a.py
@@ -0,0 +1,13 @@
+num: int = int(input())
+lst: list[str] = input().split()
+lst: list[int] = list(map(lambda x: int(x), lst))
+max_num: int = lst[0]
+max_i: int = 0
+min_num: int = lst[0]
+min_i: int = 0
+for i in range(len(lst)):
+    if lst[i] >= max_num:
+        max_i = i
+    if lst[i] < min_num:
+        min_i = i
+print(max_i + 1, min_i + 1)
