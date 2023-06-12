69015a2b97317a03213e8313680ffdf92848ba6f
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-06-12 23:54:35 +0300

    part one finish

--- Stepik/Generation_Python_course_for_beginners/2_1.py
+++ Stepik/Generation_Python_course_for_beginners/2_1.py
@@ -1,1 +1,48 @@
-# start programing 🌶️
+#Тема урока: ввод-вывод данных 🌶️
+
+# task 1
+print('Здравствуй, мир!')
+
+# task 2
+print(4,8,15,16,23,42)
+
+# task 3
+print(4)
+print(8)
+print(15)
+print(16)
+print(23)
+print(42)
+
+# task 4
+print('*')
+print('**')
+print('***')
+print('****')
+print('*****')
+print('******')
+print('*******')
+
+# task 5
+name = input()
+print('Привет,', name)
+
+# task 6
+team = input()
+print(team, "- чемпион!")
+
+# task 7
+text1 = input()
+text2 = input()
+text3 = input()
+print(text1)
+print(text2)
+print(text3)
+
+# task 8
+text1 = input()
+text2 = input()
+text3 = input()
+print(text3)
+print(text2)
+print(text1)
