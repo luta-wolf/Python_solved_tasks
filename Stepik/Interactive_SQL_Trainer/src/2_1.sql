b342a26e550c2a05ecdc804200907a90bc17b719
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-07-17 10:57:44 +0300

    add sql tuturial

---
+++ Stepik/Interactive_SQL_Trainer/src/2_1.sql
@@ -0,0 +1,14 @@
+-- 1
+CREATE TABLE author(
+    author_id SERIAL,
+    name_author VARCHAR(50)
+    );
+
+-- 2
+INSERT INTO author(name_author)
+VALUES('Булгаков М.А.'),
+('Достоевский Ф.М.'),
+('Есенин С.А.'),
+('Пастернак Б.Л.');
+
+
